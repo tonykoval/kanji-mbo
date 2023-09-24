48f4747e69c08089a973ef3ec4355605155e7bb8
patokan <116893071+patokan@users.noreply.github.com>
2023-09-24 14:55:17 +0200

    sept 23 (fixed some comps)
    紫・此, etc. + 欧

--- main.py
+++ main.py
@@ -10,28 +10,28 @@ source = algorithm.read_excel("2250 KANJI COMPONENTS - ver. 1.0.xlsx")
 categorization = algorithm.init_categorization(source)
 # {'磨', '麻', '摩'}
 
-# algorithm.categorize_kanji(algorithm.read_kanji_char("戻", source), categorization, source)
+algorithm.categorize_kanji(algorithm.read_kanji_char("欧", source), categorization, source)
 # algorithm.categorize_kanji(algorithm.read_kanji_char("麻", source), categorization, source)
 # algorithm.categorize_kanji(algorithm.read_kanji_char("摩", source), categorization, source)
 #
-for i in range(len(source.df_kanji)):
-    # print(i)
-    row = algorithm.read_kanji(source.df_kanji.loc[i])
-    # print(row.char)
-    algorithm.categorize_kanji(row, categorization, source)
-    print("-----------------")
-
-algorithm.categorize_queue(categorization)
-
-print("categorization")
-for key in categorization.result:
-    print(key)
-    for kanji in categorization.result[key]:
-        print(kanji.char)
-
+# for i in range(len(source.df_kanji)):
+#     # print(i)
+#     row = algorithm.read_kanji(source.df_kanji.loc[i])
+#     # print(row.char)
+#     algorithm.categorize_kanji(row, categorization, source)
+#     print("-----------------")
+#
+# algorithm.categorize_queue(categorization)
+#
+# print("categorization")
+# for key in categorization.result:
+#      print(key)
+#      for kanji in categorization.result[key]:
+#         print(kanji.char)
+#
 # print("queue_categorization")
 # for key in categorization.queue:
-#     print("key: ", key)
-#     for kanji in categorization.queue[key]:
-#         print(kanji.char)
-#     print("----")
+#      print("key: ", key)
+#      for kanji in categorization.queue[key]:
+#          print(kanji.char)
+#      print("----")
