bcd0b3fb4e45df3e83d600ec2b2057ac25819080
tonykoval <tony@tonykoval.com>
2024-12-27 18:39:03 +0100

    update

--- freq/main.py
+++ freq/main.py
@@ -1,8 +1,12 @@
 import logging
+from operator import attrgetter
+
+from disjoint_set import DisjointSet
 
 import freq.algorithm as algorithm
 
 import sys
+
 sys.stdout = open('output.txt', 'wt', encoding="utf-8")
 
 algorithm.set_logging_level(logging.INFO)
@@ -13,10 +17,32 @@ result = []
 for kanji in list_kanji:
     algorithm.categorize_kanji(kanji, result, list_kanji)
 
+ds = DisjointSet()
+
+for k in result:
+    ds.union(k.char, k.ref)
+
+print(ds)
+
 num = 1
-for res in result:
-    print(f"{num}: {res.char} -> {res.ref}")
-    num +=1
+for r in ds.itersets(with_canonical_elements=False):
+    # ds.find(r.char)
+    res = []
+    for ch in r:
+        for k in list_kanji:
+            if k.char == ch:
+                res.append(k)
+                break
+
+    sorted_people = sorted(res, key=attrgetter('freq'))
+
+    for ch in sorted_people:
+        print(f'{num}: {ch.char}')
+        num += 1
+    # print(r)
+    # for k, v in r:
+    #     print(k, v)
+    # print(f'{num}: {r.}')
 
 # for i in range(len(list_kanji)):
 #     # print(i)
